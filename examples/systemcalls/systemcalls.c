00ab41e17ff9cf3be6835004d5cb172ce62974d6
Gone <mahmoud.abuelenin603789@gmail.com>
2024-03-14 02:00:47 +0200

    first

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,13 @@
 #include "systemcalls.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <signal.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -9,30 +17,20 @@
 */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+	if (cmd != NULL)
+	{
+		int ret = system(cmd);
+		if(	(ret==-1)&&(ret==127) )
+		{
+			return false;
+		}
+		else
+		{
+			return true ;
+		}
+	}
+	return false ;
 }
-
-/**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
 
 bool do_exec(int count, ...)
 {
@@ -45,30 +43,31 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
-
-    va_end(args);
+	int status;
+	pid_t pid;
+	pid = fork();
+	
+	if (pid == -1){
+		va_end(args);
+		return false;}
+	
+	else if (pid == 0) 
+	{
+		if (execv(command[0], command) == -1)
+				exit(-1);
+	}
+	
+	else
+	{
+		waitpid(pid, &status, 0);
+		va_end(args);
+		return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+	}
 
     return true;
 }
 
-/**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
@@ -78,22 +77,47 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
+		printf("%s\n",command[i]);
     }
+	//printf("%s\n",*command);
+
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
 
+	//execlp( "/bin/sh", "/bin/sh", "-c", "%s > %s",command[0],outputfile, (char *)NULL );
+	
+	int status;
+	pid_t pid;
+	pid = fork();
+	
+	if (pid == -1){
+		va_end(args);
+		return false;}
+	
+	else if (pid == 0) 
+	{
+		int fd = open(outputfile, O_WRONLY | O_CREAT, 0644 );
+		if (fd == -1){
+			exit(-1);
+		}
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+		if (dup2(fd,STDOUT_FILENO) == -1){
+			exit(-1);
+		}
+		close(fd);
 
-    va_end(args);
+		if(execv(command[0], command) == -1)
+			exit(-1);
 
-    return true;
+	}
+	else
+		{
+			waitpid(pid, &status, 0);
+			va_end(args);
+			return (WEXITSTATUS(status) && WIFEXITED(status) == 0);
+		}
+
+		return true;
+
 }
